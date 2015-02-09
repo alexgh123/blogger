710c2f9dceaccfdf7118bade413d6ea42257f82e
Alex Hardt <hardt.alex@gmail.com>
2015-02-09 11:23:16 -0600

    form based workflow workflow completed...flash notices too

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'articles#index' #this line changes url of page from default 'index' to 'articles...idk why the name of the string matters/how its used'
   resources :articles
 end
