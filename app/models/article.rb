75c06d12788f0e0b075d76728fc9d0ab537914b3
Alex Hardt <hardt.alex@gmail.com>
2015-02-10 08:04:57 -0600

    added tag feature on app(notes below)
    notes also in article.rb file, copied here:
    
      #^the rails form tag helper calls an attribute by the same name on the model. active record creates methods to read attributes, but 'tag list' is not a column/attribute on model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ActiveRecord::Base
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(", ")
+  end #this is where the rails magic ended, there are active record methods that rails can use for title, and body, but not 'tag list', so in the article.rb model (this file), you have to define the method so rails knows what to do with it(what is it)
+
+  #^the rails form tag helper calls an attribute by the same name on the model. active record creates methods to read attributes, but 'tag list' is not a column/attribute on model
+
+
 end
